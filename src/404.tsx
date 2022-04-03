f02eea3d1a2a2441b86f41f00ec3a215c6858466
Erik Vroon <erik.vroon22@gmail.com>
2022-04-03 15:03:50 +0000

    Fix 404 page

---
+++ src/404.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client';
+import App404 from './components/404';
+
+const container404 = document.getElementById('root404')!;
+ReactDOM.createRoot(container404).render(<App404 />);
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
 import App404 from './components/404';
