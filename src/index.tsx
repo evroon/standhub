859f634e514e3d228060137bfe7b0cd7eac72cf3
Erik Vroon <erik.vroon22@gmail.com>
2022-04-03 14:08:41 +0000

    Add 404 page

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
+import App404 from './components/404';
 
 const container = document.getElementById('root')!;
 ReactDOM.createRoot(container).render(<App />);
+
+const container404 = document.getElementById('root404')!;
+ReactDOM.createRoot(container404).render(<App404 />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
