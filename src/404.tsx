0e7a670c7513cd403daa5e76b59d3cd5bbd82db3
Erik Vroon <erik.vroon22@gmail.com>
2022-04-03 15:17:21 +0000

    Handle 404 in react-router

--- src/404.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App404 from './components/404';
-
-const container404 = document.getElementById('root404')!;
-ReactDOM.createRoot(container404).render(<App404 />);
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import ReactDOM from 'react-dom/client';
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import App from './app';
-import reportWebVitals from './reportWebVitals';
 import App404 from './components/404';
 
+const RouterApp = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App/>} />
+            <Route path="*" element={<App404/>} />
+        </Routes>
+    </BrowserRouter>
+);
+
 const container = document.getElementById('root')!;
-ReactDOM.createRoot(container).render(<App />);
-
-const container404 = document.getElementById('root404')!;
-ReactDOM.createRoot(container404).render(<App404 />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.createRoot(container).render(<RouterApp />);
